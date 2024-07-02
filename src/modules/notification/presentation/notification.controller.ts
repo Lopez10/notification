6936c836cc6f9936c4b35991aa9d5d72fad820dd
Juan LB <ljuan7397@gmail.com>
2024-07-02 14:48:59 +0200

    feat: Add message pattern for notification creation

--- src/modules/notification/presentation/notification.controller.ts
+++ src/modules/notification/presentation/notification.controller.ts
@@ -1,12 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { CreateNotification } from '../application/create-notification.use-case';
 import { MessagePattern, Payload } from '@nestjs/microservices';
+import { NOTIFICATION_CREATED } from 'src/patterns';
 
 @Controller()
 export class NotificationController {
 	constructor(private readonly notificationService: CreateNotification) {}
 
-	@MessagePattern('notification_created')
+	@MessagePattern(NOTIFICATION_CREATED)
 	run(@Payload() data: Record<string, unknown>) {
 		this.notificationService.handleNotification({ message: data });
 	}
